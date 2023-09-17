6c64ca54bf9cedbf73b37c537815f967fafa2b02
Rafael Reis <rafyangtj@gmail.com>
2023-09-17 12:03:01 -0300

    added tabs-components

--- src/common/template/footer.jsx
+++ src/common/template/footer.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default props => (
    <footer className="main-footer">
       <strong>
-         Copyright & Copy 2017
-         <a href="" target="_blank">Cod3r</a>.
+         Copyright & Copy 2017 -
+         <a href="" target="_blank"> Cod3r</a>
       </strong>
    </footer>
 )
